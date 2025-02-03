75e9332803ecd2895116794d8d68cee22239292c
E.N <67209588+ElisNz@users.noreply.github.com>
2025-02-03 17:23:34 +0100

    added image files and main page updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,18 +11,18 @@ export default function Home() {
                 <h1 className='invisible sm:visible lg:px-lg lg:text-[2.5rem] xl:text-[3.5rem]'>Winter gardens for modern spaces</h1>
                 <p className="hidden lg:visible py-sm l wrap-balance text-[1.3rem]">We build super light modular structures that can weather all seasons. <br></br> Our goal is to blend the joy of being outside with the comforts of being inside. </p>
             </div>
-            <div className='flex flex-col gap-8 justify-center items-center m-lg p-md'>
-              <h3 className='w-1/2 tracking-tight'>We build super light modular structures that bridge the gap between indoors and outdoors. Our affordable designs are engineered to last but easy to change and customize.</h3>
-              <Link href="/projects" className='w-fit bg-black text-white p-sm hover:bg-black/50 self-center rounded-sm'>Discover our projects</Link>
+            <div className='flex flex-col gap-8 justify-center items-center m-lg py-md px-xl'>
+              <h2>We build super light modular structures that bridge the gap between indoors and outdoors. Our affordable designs are engineered to last but easy to change and customize.</h2>
+              <Link href="/projects" className='w-1/2 bg-black/70 text-white text-center p-sm hover:bg-black/50 self-center rounded-sm'>Discover our projects</Link>
             </div>
             <div className='flex flex-row gap-8 lg:gap-12 bg-primary justify-evenly mx-lg mt-lg py-lg px-lg'>
                 <div className='w-2/3 flex flex-col'>
                     <h2 className='text-pretty pt-4 pb-2 text-left tracking-tight'>"Motherstructures has created a beautiful and super unique solution that perfectly complements who we are. Our courtyard has been transformed into a three-season dining room that wows our customers."</h2>
                     <p className='italic pb-8 text-right tracking-tighter'>Jeff Gimmel, Proprietor, Le Perche Bakery & Bar</p>
-                    <Link href="/about" className='w-fit bg-black text-white px-sm py-2 hover:bg-black/50 rounded-sm'>Learn More</Link>
+                    <Link href="/about" className='w-1/2 bg-black/70 text-white text-center p-sm hover:bg-black/50 rounded-sm'>Learn More</Link>
                 </div>
                 <div className='relative w-1/2'>
-                  <Image src='/images/Hudson_Clothier_ 5.jpg' fill alt='outdoors' className='lg:rounded-sm object-cover'/>
+                  <Image src='/images/Le Perche.jpg' fill alt='outdoors' className='lg:rounded-sm object-cover'/>
                 </div>
             </div>
             <div className="mx-lg mt-lg">
@@ -67,17 +67,24 @@ export default function Home() {
                         </li>
                     </ul>
                 </div>
-                <div className='flex flex-col gap-8 m-lg py-md px-xl bg-secondary'>
+                <div className='flex flex-col gap-8 m-lg py-lg px-xl bg-secondary'>
                   <div>
                     <h1>Lightweight, strong and versatile</h1>
-                    <p>Due to our inovative use of simple materials, we can create almost any kind of structure, affordably and sustainably.</p>
+                    <p className='pt-sm'>Due to our inovative use of simple materials, we can create almost any kind of structure, affordably and sustainably. 
+                        The modularity of the design means that the structures are easy to modify and maintain.
+                    </p>
                   </div>
-                  <Link href="/about" className='w-fit bg-black text-white p-4 hover:bg-black/50 rounded-sm'>Explore our design components</Link>
+                  <div className='relative w-full h-[50vh] self-end'>
+                    <Image src='/images/230609_Le_Perche_ 15.jpg' fill alt='outdoors' className='lg:rounded-sm object-cover'/>
+                  </div>
+                  <Link href="/about" className='w-1/2 self-center bg-black/70 text-white text-center p-4 hover:bg-black/50 rounded-sm'>Explore our design components</Link>
                 </div>
                 <div className='flex flex-col items-center gap-8 m-lg py-md'>
                   <h1>Contact us</h1>
-                  <p>Get started with your own Motherstructure</p>
-                  <Link href="/about" className='w-1/4 bg-black text-white text-center p-sm hover:bg-black/50 rounded-sm'>Get in touch</Link>
+                  <p>Curious about what your very own Motherstructure could look like? <br />
+                    Tell us about your space and we will send some ideas.
+                  </p>
+                  <Link href="/about" className='w-1/4 bg-black/70 text-white text-center p-sm hover:bg-black/50 rounded-sm'>Get in touch</Link>
                 </div>
             </main>
 	);
